ffde60df4c860e7e77dc0f9caba67d84806f34b1
SamFabra <samuel.fabra@ontariotechu.net>
2022-11-10 15:38:05 -0500

    added steering behaviour -seek

---
+++ Assets/Scripts/SteeringBehaviours.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteeringBehaviours : MonoBehaviour
+{
+    public List<GameObject> targets = new List<GameObject>();
+
+    float time = 0f;
+    public float changeTime = 3f;
+    int index = 0;
+
+    [Range(0, 5)]
+    public float speed = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        if (time > changeTime)
+        {
+            index = Random.Range(0, targets.Count);
+            time = 0f;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, targets[index].transform.position, speed * Time.deltaTime);
+
+        transform.LookAt(targets[index].transform.position);
+    }
+}
